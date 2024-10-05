4cce6281fa120e422355987531ddb5b85777ef40
mizunomu1v1 <fulltime.furotime@gmail.com>
2024-10-05 15:50:53 +0900

    20241005_01

---
+++ 2024/コーディング基礎A/Practice01_FizzBuzz.java
@@ -0,0 +1,18 @@
+public class Practice01_FizzBuzz {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 1; i <= 15; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
