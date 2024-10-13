dee8f9cb85d119f260a8868116b50d451b6a3239
mizunomu1v1 <fulltime.furotime@gmail.com>
2024-10-13 17:58:16 +0900

    テーブル作成スクリプト完成！

--- 2024/がくしゅう帳/ツール/table_create.ps1
+++ 2024/がくしゅう帳/ツール/table_create.ps1
@@ -3,45 +3,66 @@
 # 作成日：2024/10/11
 #--------------------------------- #>
 
-$returnPath=$PWD
-$workPath="C:\file"
-cd $workPath
+# 作業フォルダ移動
+$returnPath = $PWD
+$workPath = "C:\file"
+Set-Location $workPath
 
-$logPath="$workPath\log\create_table_log.txt"
-$inputPath="$workPath\input\table.csv"
-$outputPath="$workPath\output\"
+# パス定義
+$logPath = "$workPath\log\create_table_log.txt"
+$inputPath = "$workPath\input\table.csv"
+$outputPath = "$workPath\output\"
 
-$sysdate = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-
-# --------------------------------------------------
-
-# 開始ログ
-Write-Output "$sysdate 処理開始" | Out-File -Append $logPath
+# 開始ログ出力
+$startSysdate = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+Write-Output "$startSysdate 処理開始" | Out-File -Append $logPath
 
 # .csv読み込み
-$table = Import-Csv $inputPath -Encoding UTF8
-$table | Format-Table
-$tableName = $table[1].table_name
-$outputFile="$outputPath$tableName.sql"
+$datas = Import-Csv $inputPath -Encoding UTF8
+$datas | Format-Table # 標準出力に表形式で出力
+$tableName = $datas[1].table_name
+$outputFile = Join-Path -Path $outputPath -ChildPath "$tableName.sql"
 
 # .sql書き込み
 Write-Output "CREATE TABLE $tableName (" | Out-File $outputFile
 
-cd $returnPath
+# カラムを設定する
+$columns = @()
+foreach ($data in $datas) {
+	if (-not [string]::IsNullOrEmpty($data.max_length)) {
+		$columns += "$($data.column_name) $($data.column_type)($($data.max_length)),"
+	}
+	else {
+		$columns += "$($data.column_name) $($data.column_type),"
+	}
+}
 
-# CSVファイルの読み込み
-# for /f "skip=1 delims=, tokens=1-3" %%a in (%input%) do ( 
-# 	echo 名前　　:%%a
-# 	echo 生年月日:%%b
-# 	echo 性別　　:%%c
-# 	echo.
-# )
+# カラムをカンマで結合し出力
+$columnText = $columns -join "`r`n   "
+Write-Output "   $columnText" | Out-File -Append $outputFile
 
+# 複合主キーを設定する
+foreach ($data in $datas) {
+	if (-not [string]::IsNullOrEmpty($data.pk)) {
+		$pks += $data.column_name
+	}
+}
+# 主キーをカンマで結合し出力
+$pkText = $pks -join ", "
+Write-Output "   PRIMARY KEY($($pkText))" | Out-File -Append $outputFile
+Write-Output "   ); " | Out-File -Append $outputFile
+	
+# コメントを設定する
+$comments = $datas | ForEach-Object {
+	"COMMENT ON COLUMN $tableName.$($_.column_name) IS '$($_.remarks)'; "
+}
+# コメントを結合し出力
+$commentText = $comments -join "`r`n"
+Write-Output "$commentText" | Out-File -Append $outputFile
 
-# 入力CSVファイル読み込み
-# echo "CREATE TABLE" %table_name% "(" >> %$output%
+# 元のフォルダに戻る
+Set-Location $returnPath
 
-# create table mybook (
-#   id integer, 
-#   name varchar(10)
-# );
+# 終了ログ出力
+$endSysdate = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+Write-Output "$endSysdate 処理終了" | Out-File -Append $logPath
